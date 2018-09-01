9effa2ba90c78ad6abe22124c6120b4e11987776
Ryledra <vince@nadin.one>
2018-09-01 15:21:22 +0100

    .gitignore's removed

--- Printing/src/Runner.java
+++ Printing/src/Runner.java
@@ -4,13 +4,10 @@ public class Runner {
 	public static void main(String[] args) {
 		String str = "hello my friends";
 		Printing print = new Printing();
-		
-		print.printing1(str);
-		System.out.println();
-		print.printing2(str);
-		System.out.println();
-		print.printingWithEquals(str);
 
+		print.printing1(str);
+		System.out.println("\n");
+		print.printing2(str);
 	}
 
 }
