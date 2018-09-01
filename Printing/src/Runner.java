289c433f12f3cc78c1f88ef65bce926a55a7478b
Ryledra <vince@nadin.one>
2018-09-01 15:54:45 +0100

    user defined exception updated

--- Printing/src/Runner.java
+++ Printing/src/Runner.java
@@ -4,16 +4,16 @@ public class Runner {
 	public static void main(String[] args) {
 		String str = "hello my friends";
 		Printing print = new Printing();
-
-		print.printing1(str);
-		System.out.println("\n");
-		print.printing2(str);
 		
 		
 		try {
 			if (str.split(" ")[0].equals("hello")) {
 				QAEx qe = new QAEx();
 				throw qe;
+			} else	{
+				print.printing1(str);
+				System.out.println("\n");
+				print.printing2(str);
 			}
 		} catch (QAEx qe) {
 			System.out.println(qe);
